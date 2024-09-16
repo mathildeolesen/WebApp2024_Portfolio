2fbf4e01083ea402e4fbb6f0cbd7f43a5e6d808f
Mathilde <111729553+mathildeolesen@users.noreply.github.com>
2024-09-16 13:35:11 +0200

    Mapper Experience-komponenter fra "experience" i studentens objekt
    * ExperienceSection
    - Lager en type Experience, sånn at ExperienceProps kan si at det er en liste med denne typen ( Experience[ ] ) vi forventer.

--- portfolio-v2/src/App.tsx
+++ portfolio-v2/src/App.tsx
@@ -11,14 +11,25 @@ function App() {
       lastName: "Olesen",
       degree: "Bachelor i Informatikk",
       degreeFinished: 2025,
-      email: "mathildeolesen@gmial.com"
+      email: "mathildeolesen@gmial.com",
+
+      experiences: [
+        { 
+          title: "Brukerbasert Film-kolleksjon nettside med React og Sanity", 
+          description: 'Nettside hvor brukere kan lage kolleksjoner med favoritter, sett og "ønsker-å-se" filmer, så brukere kan sammen finne en film begge liker/har interesse av å se'
+        },
+        { 
+          title: "Havn-simulering rammeverk i .NET med C#", 
+          description: 'Et rammeverk for å simulere havnaktivitet som av- og pålessing av containere, utsending av båter, levering av containere fra havnen, m.m. Laget med .NET og C#.'
+        }
+      ]
     }
 
   return (
     <main>
       <WelcomeCard student={dummyStudent}/>
       <ProjectSection/>
-      <ExperienceSection/>
+      <ExperienceSection experiences={dummyStudent.experiences}/>
     </main>
   );
 }
--- portfolio-v2/src/components/ExperienceSection.tsx
+++ portfolio-v2/src/components/ExperienceSection.tsx
@@ -1,19 +1,15 @@
 import Experience from "./Experience";
 
-export default function ExperienceSection() {
+type Experience = {
+    title: string;
+    description: string;
+};
 
+type ExperiencesProps = {
+    experiences: Experience[];
+};
 
-    const dummyExperience1 = 
-    { 
-        title: "Brukerbasert Film-kolleksjon nettside med React og Sanity", 
-        description: 'Nettside hvor brukere kan lage kolleksjoner med favoritter, sett og "ønsker-å-se" filmer, så brukere kan sammen finne en film begge liker/har interesse av å se'
-    }
-
-    const dummyExperience2 = 
-    { 
-        title: "Havn-simulering rammeverk i .NET med C#", 
-        description: 'Et rammeverk for å simulere havnaktivitet som av- og pålessing av containere, utsending av båter, levering av containere fra havnen, m.m. Laget med .NET og C#.'
-    }
+export default function ExperienceSection({ experiences }: ExperiencesProps) {
 
     return (
         <section id="display">
@@ -21,8 +17,9 @@ export default function ExperienceSection() {
                 <h2>Erfaringer</h2>
             </div>
             <div id="projectsContainer">
-                <Experience experience={dummyExperience1}/>
-                <Experience experience={dummyExperience2}/>
+                {experiences.map((experience) => (
+                    <Experience experience={experience}/>
+                ))}
             </div>
         </section>
     )
