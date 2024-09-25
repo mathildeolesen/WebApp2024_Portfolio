e3db025f8e4e901240c80707b23f3364224b056c
Mathilde <111729553+mathildeolesen@users.noreply.github.com>
2024-09-25 17:50:18 +0200

    Validering av kontaktskjema input e-post, emne og melding

--- portfolio-v2/src/components/Contact.tsx
+++ portfolio-v2/src/components/Contact.tsx
@@ -1,16 +1,14 @@
 import { FormEvent, useState } from "react";
 
-type Email = `${string}@${string}.${string}`;
-
 
 type ContactProps = {
-    email: Email;
+    email: string;
 }
 
 type Message = {
     id: string,
     senderName: string,
-    senderEmail: Email,
+    senderEmail: string,
     topic: string,
     message: string
 }
@@ -22,9 +20,41 @@ export default function Contact(props: ContactProps) {
     const { email } = props;
 
     const [senderName, setSenderName] = useState("");
-    const [senderEmail, setSenderEmail] = useState<Email>("" as Email);
+    const [senderEmail, setSenderEmail] = useState("");
     const [topic, setTopic] = useState("");
     const [message, setMessage] = useState("");
+
+    const [senderEmailIsValid, setSenderEmailIsValid] = useState(false)
+    const [senderEmailIsTouched, setSenderEmailIsTouched] = useState(false);
+    const [senderEmailIsDirty, setSenderEmailIsDirty] = useState(false);
+
+    const [topicIsValid, setTopicIsValid] = useState(false)
+    const [topicIsTouched, setTopicIsTouched] = useState(false);
+    const [topicIsDirty, setTopicIsDirty] = useState(false);
+    
+    const [messageIsValid, setMessageIsValid] = useState(false)
+    const [messageIsTouched, setMessageIsTouched] = useState(false);
+    const [messageIsDirty, setMessageIsDirty] = useState(false);
+
+    const validateEmailInput = (email: string) => {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        
+        if (senderEmailIsTouched && senderEmailIsDirty) {
+            setSenderEmailIsValid(emailRegex.test(email));
+        }
+    }
+
+    const validateTopicInput = (topic: string) => {
+        if (topicIsTouched && topicIsDirty) {
+            setTopicIsValid(topic.trim().length > 3)
+        }
+    }
+
+    const validateMessageInput = (message: string) => {
+        if (messageIsTouched && messageIsDirty) {
+            setMessageIsValid(message.trim().split(/\s+/).length > 2)
+        }
+    }
 
     const updateSenderName = (event: FormEvent<HTMLInputElement>) => {
         const input = event.target as HTMLInputElement | null;
@@ -35,20 +65,22 @@ export default function Contact(props: ContactProps) {
     const updateSenderEmail = (event: FormEvent<HTMLInputElement>) => {
         const input = event.target as HTMLInputElement | null;
         if (!input) return;
-
-        setSenderEmail(input.value as Email);
+        setSenderEmailIsDirty(true);
+        setSenderEmail(input.value);
     };
     
     const updateTopic = (event: FormEvent<HTMLInputElement>) => {
         const input = event.target as HTMLInputElement | null;
         if (!input) return;
-        setTopic(input.value)
+        setTopicIsDirty(true);
+        setTopic(input.value);
     };
 
     const updateMessage = (event: FormEvent<HTMLInputElement>) => {
         const input = event.target as HTMLInputElement | null;
         if (!input) return;
-        setMessage(input.value)
+        setMessageIsDirty(true);
+        setMessage(input.value);
     };
     
     
@@ -71,12 +103,11 @@ export default function Contact(props: ContactProps) {
         ])
 
         setSenderName("");
-        setSenderEmail("" as Email);
+        setSenderEmail("");
         setTopic("");
         setMessage("");
         
     };
-
 
     return (
         <section>
@@ -88,28 +119,52 @@ export default function Contact(props: ContactProps) {
                     2
                 )}
             </pre>
+
             <form id="contactForm" onSubmit={addProject}>
-            <div id="senderName">
-              <label htmlFor="senderName">Navn</label>
-              <input type="text" id="senderName" name="senderName" 
-                onChange={updateSenderName} value={senderName} required/>
-            </div>
-            <div id="senderEmail">
-              <label htmlFor="senderEmail">Din e-post</label>
-              <input type="text" id="senderEmail" name="senderEmail" 
-              onChange={updateSenderEmail} value={senderEmail} required/>
-            </div>
-            <div id="topic">
-              <label htmlFor="topic">Emne</label>
-              <input type="text" id="topic" name="topic" 
-              onChange={updateTopic} value={topic} required/>
-            </div>
-            <div id="message">
-              <label htmlFor="message">Melding</label>
-              <input type="textarea" id="message" name="message"
-                onChange={updateMessage} value={message} required/>
-            </div>
-            <button type="submit">Send melding</button>
+                <div id="senderName">
+                    <label htmlFor="senderName">Navn</label>
+                    <input type="text" id="senderName" name="senderName" 
+                    onChange={updateSenderName}
+                    value={senderName} required/>
+                </div>
+
+                <div id="senderEmail">
+                    <label htmlFor="senderEmail">Din e-post</label>
+                    <input type="text" id="senderEmail" name="senderEmail" 
+                    onChange={updateSenderEmail}
+                    onFocus={() => { setSenderEmailIsTouched(true) }}
+                    onBlur={() => {validateEmailInput(senderEmail)}}
+                    value={senderEmail} required/>
+                    {!senderEmailIsValid && senderEmailIsDirty ? (
+                        <p className="warning">E-posten er ikke gyldig 🙂</p>
+                    ) : null}
+                </div>
+
+                <div id="topic">
+                    <label htmlFor="topic">Emne</label>
+                    <input type="text" id="topic" name="topic" 
+                    onChange={updateTopic}
+                    onFocus={() => { setTopicIsTouched(true) }}
+                    onBlur={() => { validateTopicInput(topic) }}
+                    value={topic} required/>
+                    {!topicIsValid && topicIsDirty ? (
+                        <p className="warning">Emne må være lengre enn 3 tegn 🙂</p>
+                    ) : null}
+                </div>
+
+                <div id="message">
+                    <label htmlFor="message">Melding</label>
+                    <input type="textarea" id="message" name="message"
+                    onChange={updateMessage}
+                    onFocus={() => { setMessageIsTouched(true) }}
+                    onBlur={() => { validateMessageInput(message) }}
+                    value={message} required/>
+                    {!messageIsValid && messageIsDirty ? (
+                        <p className="warning">Meldingen må være mer enn 2 ord 🙂</p>
+                    ) : null}
+                </div>
+
+                <button type="submit">Send melding</button>
           </form>
         </section>
     )
