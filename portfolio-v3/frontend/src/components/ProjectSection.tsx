894b520e50870017a9f79d9699f83873cfd33f6b
Mathilde <111729553+mathildeolesen@users.noreply.github.com>
2024-10-19 15:39:12 +0200

    frontend - Start av implementering av useProjects i ProjectSection
    * Mangler å få ProjectCard til å også ta i bruk useProjects
    - (bytte ut project, setProject med useProject logikken)

--- portfolio-v3/frontend/src/components/ProjectSection.tsx
+++ portfolio-v3/frontend/src/components/ProjectSection.tsx
@@ -1,86 +1,66 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import AddProjectForm from "./AddProjectForm";
 import ProjectCard from "./ProjectCard";
-import { ofetch } from "ofetch";
-
-type Project = {
-
-  id: string, // UUID er en streng
-  title: string,
-  tags: string[],
-  description: string
-}
+import useProjects from "@/features/projects/hooks/useProjects"; // Bruker useProjects-hook
 
 export default function ProjectSection() {
+  const { status, get, data, error } = useProjects();
+  const { projects } = data; // Destrukturerer prosjektene fra data
+  const [showForm, setShowForm] = useState(false);
 
-    const [projects, setProjects] = useState<Project[]>([]);
-    const [showForm, setShowForm] = useState(false);
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  };
 
-    const toggleForm = () => {
-        setShowForm(!showForm);
-    }
+  const countTags = (): Map<string, number> => {
+    const tagCounts = new Map<string, number>();
 
-    const initializeData = () => {
-      ofetch("http://localhost:3000/projects").then(
-        (projects: { data: Project[]}) => {
-          
-          setProjects(projects.data || []);
-      })
-    };
-
-    useEffect(() => {
-      initializeData();
-    }, [])
-
-
-    const countTags = (): Map<string, number> => {
-      const tagCounts = new Map<string, number>();
-  
-      projects.forEach((project) => {
-        project.tags.forEach((tag) => {
-          tagCounts.set(tag, (tagCounts.get(tag.toLowerCase()) || 0) + 1);
-        });
+    projects.forEach((project) => {
+      project.tags.forEach((tag) => {
+        tagCounts.set(tag, (tagCounts.get(tag.toLowerCase()) || 0) + 1);
       });
+    });
+
+    return tagCounts;
+  };
   
-      return tagCounts;
-    };
-    
-    const tagCounts = countTags();
+  const tagCounts = countTags();
 
-    return (
+  return (
 
-        <section id="display">
-        <div id="displayHeader">
-          <h2>Prosjekter</h2>
-          <button id="showFormButton" onClick={toggleForm}>Legg til prosjekt</button>
-        </div>
-        <div id="projectsContainer">
-          <div id="tagCount">
-            <p>Antall prosjekter per tag:</p>
-            <div id="countedTags">
-              {Array.from(tagCounts.entries()).map(([tag, count]) => (
-              <p key={tag}>
-                {tag.charAt(0).toUpperCase() + tag.slice(1)}: {count}
-              </p>
-            ))}
-            </div>
+      <section id="display">
+      <div id="displayHeader">
+        <h2>Prosjekter</h2>
+        <button id="showFormButton" onClick={toggleForm}>Legg til prosjekt</button>
+      </div>
+      <div id="projectsContainer">
+        <div id="tagCount">
+          <p>Antall prosjekter per tag:</p>
+          <div id="countedTags">
+            {Array.from(tagCounts.entries()).map(([tag, count]) => (
+            <p key={tag}>
+              {tag.charAt(0).toUpperCase() + tag.slice(1)}: {count}
+            </p>
+          ))}
           </div>
-        {projects.length === 0 
-        ? (
-          <article className="displayCard">
-            <div id="displayCardInfo">
-              <h3>Ingen prosjekter 🥲</h3>
-            </div>
-          </article>
-        ) : (
-          projects.map((project, index) => (
-            <ProjectCard key={index} project={project} setProjects={setProjects}/>
-        ))
-        )}
-        
         </div>
-        {showForm && <AddProjectForm setProjects={setProjects} toggleForm={toggleForm}/>}
-        {/* If showForm is true, render component */}
-      </section>
-    )
+      {projects.length === 0 
+      ? (
+        <article className="displayCard">
+          <div id="displayCardInfo">
+            <h3>Ingen prosjekter 🥲</h3>
+          </div>
+        </article>
+      ) : (
+        projects.map((project, index) => (
+          <ProjectCard key={index} project={project} setProjects={setProjects}/>
+      ))
+      )}
+      
+      </div>
+      {showForm && <AddProjectForm setProjects={setProjects} toggleForm={toggleForm}/>}
+      {/* If showForm is true, render component */}
+    </section>
+  )
 }
+
